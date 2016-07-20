aa5070173bbb7f12114054125bd2df7f51726702
Paulo Elias Ignacio Piñol <oluapsaile2000@gmail.com>
2016-07-20 15:09:25 +0800

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,10 +5,12 @@ exports.config = {
   // The address of a running selenium server.
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
-
+  //added comment here to add lines
+  
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'browserName': 'firefox'
   },
 
   baseUrl: 'http://localhost:3000',
@@ -18,7 +20,7 @@ exports.config = {
   specs: ['test/protractor/**/*.js'],
 };
 
-//hello world
+//hello world, hello no
 //this is a comment
 
-//another comment
+//another comment + another 1 ugh
